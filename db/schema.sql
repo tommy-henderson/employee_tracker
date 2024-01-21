0068625f5b4dded35a802e93fed279093286e410
Tommy Henderson <tommyhenderson@Tommys-MBP.ht.home>
2024-01-21 13:36:20 -0600

    changed SCHEMA and SEED files so foreign key relationships are working

--- db/schema.sql
+++ db/schema.sql
@@ -11,20 +11,22 @@ CREATE TABLE department (
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
+    salary DECIMAL,
+    department_id INT,
     FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    REFERENCES department(id) 
+    ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    FOREIGN KEY (role_id)
-    REFERENCES role(id),
+    role_id INT,
     manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id), 
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
+    ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,33 @@
 INSERT INTO department (name)
-VALUES  ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+VALUES  ("Engineering"),
+        ("Finance"),
+        ("Sales"),
+        ("Legal");
 
 INSERT INTO role (title, salary, department_id)
-VALUES  ("Account Rep", 120000, 1),
-        ("Sales Consultant", 80000, 1),
-        ("Business Development Manager", 60000, 1),
-        ("Project Leader", 170000, 2),
-        ("Software Engineer", 140000, 2),
-        ("Hardware Engineer", 90000, 2),
-        ("Financial Manager", 200000, 3),
-        ("Credit Analyst", 175000, 3),
-        ("Risk Analyst", 95000, 3),
-        ("Litigator", 250000, 4),
-        ("Legal Counsel", 170000, 4),
-        ("Paralegal", 50000, 4);
+VALUES  ("Principal Engineer", 145000, 1),
+        ("Project Engineer", 90000, 1),
+        ("Apprentice", 80000, 1),
+        ("Finance Director", 130000, 2),
+        ("Analyst", 70000, 2),
+        ("Intern", 50000, 2),
+        ("Account Executive", 150000, 3),
+        ("Sales Manager", 95000, 3),
+        ("Business Development Rep", 70000, 3),
+        ("Litigator", 200000, 4),
+        ("Legal Counsel", 180000, 4),
+        ("Paralegal", 90000, 4),
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ("Sleve", "McDichael", 1, null),
-        ("Onson", "Sweemey", 2, 1),
-        ("Darryl", "Archideld", 4, null),
-        ("Anatoli", "Smorin", 5, 4),
-        ("Rey", "McSriff", 7, 4),
-        ("Mario", "McRlwain", 1, null),
-        ("Kevin", "Nogilny", 1, 1),
-        ("Bobson", "Dugnutt", 1, 1);
+VALUES  ("Sleve", "McDichael",  1, NULL),
+        ("Onson", "Sweemey",  2, 1),
+        ("Darryl", "Archideld",  3, 1), 
+        ("Anatoli", "Smorin",  4, NULL), 
+        ("Rey", "McSriff",  5, 4),
+        ("Glenallen", "Mixon",  6, NULL), 
+        ("Mario", "McRlwain",  7, 6), 
+        ("Raul", "Chamgerlain",  8, 6), 
+        ("Kevin", "Nogilny",  9, NULL), 
+        ("Tony", "Smehrik",  10, 9), 
+        ("Bobson", "Dugnutt",  11, NULL), 
+        ("Willie", "Dustice",  12, 11);
